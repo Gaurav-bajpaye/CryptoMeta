8fd0c34bc6f47b700a96c64a8c1b5167a5cfe1d5
Gaurav-bajpaye <gauravbajpaye@gmail.com>
2025-04-27 19:31:10 +0530

    Add devDependencies for Babel plugin and update Coin components for improved functionality and styling

--- src/components/CoinChart.jsx
+++ src/components/CoinChart.jsx
@@ -1,7 +1,100 @@
-import React,{useState, useEffect} from 'react'
-import axios from 'axios'
-import { Baseurl } from './baseUrl'
-import { useParams } from 'react-router-dom'
+// import React,{useState, useEffect} from 'react'
+// import axios from 'axios'
+// import { Baseurl } from './baseUrl'
+// import { useParams } from 'react-router-dom'
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from 'chart.js';
+// import { Line } from 'react-chartjs-2';
+// import Loader from './Loader';
+
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
+// const CoinChart = ({currency}) => {
+//     const [chartData, setChartData]=useState([])
+//     const { id} = useParams()
+//     const [days, setDays]=useState(1)
+//     const CoinChartData=async()=>{
+//         try {
+//             const { data } = await axios.get(`${Baseurl}/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`)
+//             setChartData(data.prices)
+//             // console.log(data.prices)
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     }
+
+//     useEffect(()=>{
+//         CoinChartData()
+//     },[currency, id, days]);
+
+//   const myData = {
+//     labels: chartData.map((value)=>{
+//      const date = new Date(value[0])
+//      const time =
+//      date.getHours()> 12
+//      ? `${date.getHours() -12} : ${date.getMinutes()} PM`
+//      : `${date.getHours()} : ${date.getMinutes()} AM`
+//       return days===1 ? time : date.toLocaleDateString()
+//     }),
+//     datasets:[
+//         {
+//             label: ` Price in Past Days ${days} in ${currency} `,
+//             data: chartData.map((value)=>value[1]),
+//             borderColor: 'yellow',
+//             borderWidth: '3'
+//         }
+//     ]
+
+//   }
+
+//   return (
+//      <>
+//      {
+//       chartData.length === 0 ? ( <Loader/>) : (
+
+//         <div>
+//         {/* <Line data={myData} />  */}
+//         <Line data={myData} options={{
+//           elements:{
+//               point:{
+//                   radius:1,
+//               }
+//           }
+//         }} style={{marginTop:"5rem", width:"60rem"}} />
+
+//   <div className='btn' style={{marginTop:"30px"}}>
+//                <button onClick={()=>setDays(1)} >24 hours</button>
+//                <button onClick={()=>setDays(30)}>1 Month</button>
+//                <button onClick={()=>setDays(365)}>1 Year</button>
+//              </div>
+//       </div>
+//       )
+//      }
+//      </>
+//   )
+// }
+
+// export default CoinChart
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Baseurl } from "./baseUrl";
+import { useParams } from "react-router-dom";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -11,9 +104,9 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import Loader from './Loader';
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import Loader from "./Loader";
 
 ChartJS.register(
   CategoryScale,
@@ -24,73 +117,73 @@ ChartJS.register(
   Tooltip,
   Legend
 );
-const CoinChart = ({currency}) => {
-    const [chartData, setChartData]=useState([])
-    const { id} = useParams()
-    const [days, setDays]=useState(1)
-    const CoinChartData=async()=>{
-        try {
-            const { data } = await axios.get(`${Baseurl}/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`)
-            setChartData(data.prices)
-            // console.log(data.prices)
-        } catch (error) {
-            console.log(error)  
-        }
-    }
 
-    useEffect(()=>{
-        CoinChartData() 
-    },[currency, id, days]);
+const CoinChart = ({ currency }) => {
+  const [chartData, setChartData] = useState([]);
+  const { id } = useParams();
+  const [days, setDays] = useState(1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(
+          `${Baseurl}/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`
+        );
+        setChartData(data.prices);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [currency, id, days]);
 
   const myData = {
-    labels: chartData.map((value)=>{
-     const date = new Date(value[0])
-     const time = 
-     date.getHours()> 12 
-     ? `${date.getHours() -12} : ${date.getMinutes()} PM` 
-     : `${date.getHours()} : ${date.getMinutes()} AM`
-      return days===1 ? time : date.toLocaleDateString() 
-    }), 
-    datasets:[
-        {
-            label: ` Price in Past Days ${days} in ${currency} `,
-            data: chartData.map((value)=>value[1]),
-            borderColor: 'yellow',
-            borderWidth: '3' 
-        }
-    ]
-    
-  }
-
-
-
+    labels: chartData.map((value) => {
+      const date = new Date(value[0]);
+      const time =
+        date.getHours() > 12
+          ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+          : `${date.getHours()}:${date.getMinutes()} AM`;
+      return days === 1 ? time : date.toLocaleDateString();
+    }),
+    datasets: [
+      {
+        label: `Price in Past ${days} Days in ${currency}`,
+        data: chartData.map((value) => value[1]),
+        borderColor: "yellow",
+        borderWidth: 3,
+      },
+    ],
+  };
 
   return (
-     <>
-     {
-      chartData.length === 0 ? ( <Loader/>) : (
-
+    <>
+      {chartData.length === 0 ? (
+        <Loader />
+      ) : (
         <div>
-        {/* <Line data={myData} />  */}
-        <Line data={myData} options={{
-          elements:{
-              point:{
-                  radius:1, 
-              }
-          }
-        }} style={{marginTop:"5rem", width:"60rem"}} />
-  
-  <div className='btn' style={{marginTop:"30px"}}>
-               <button onClick={()=>setDays(1)} >24 hours</button>
-               <button onClick={()=>setDays(30)}>1 Month</button>
-               <button onClick={()=>setDays(365)}>1 Year</button>
-             </div>
-      </div>
-      )
-     }
-     </>
-  )
-}
+          <Line
+            data={myData}
+            options={{
+              elements: {
+                point: {
+                  radius: 1,
+                },
+              },
+            }}
+            style={{ marginTop: "5rem", width: "60rem" }}
+          />
 
-export default CoinChart
+          <div className="btn" style={{ marginTop: "30px" }}>
+            <button onClick={() => setDays(1)}>24 hours</button>
+            <button onClick={() => setDays(30)}>1 Month</button>
+            <button onClick={() => setDays(365)}>1 Year</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
+export default CoinChart;
--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,79 +1,91 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import { Baseurl } from './baseUrl'
-import Loader from './Loader'
-import axios from 'axios'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useEffect, useState } from "react";
+import { Baseurl } from "./baseUrl";
+import Loader from "./Loader";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 // import coinImage from '../coin.png'
-import './coinDetail.css'
-import { BiSolidUpArrow, BiSolidDownArrow } from "react-icons/bi"
-import { IoPulseOutline } from "react-icons/io5"
-import CoinChart from './CoinChart'
+import "./coinDetail.css";
+import { BiSolidUpArrow, BiSolidDownArrow } from "react-icons/bi";
+import { IoPulseOutline } from "react-icons/io5";
+import CoinChart from "./CoinChart";
+
+function formatISODate(isoString) {
+  const date = new Date(isoString);
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  };
+  return date.toLocaleString("en-US", options);
+}
 
 const CoinDetails = () => {
-  const [coin, setCoin]=useState([])
-  const [loading, setLoading]=useState(true)
-  const {id} = useParams()
-  const [currency, setCurrency]=useState('inr')
-  const currencySymbol = currency ==='inr' ? '₹': '$'
-  const profit = coin.market_data?.price_change_percentage_24h > 0   
-    useEffect(()=>{
-       const getCoin =async()=>{
-         try {
-           const {data} =await axios.get(`${Baseurl}/coins/${id}`)
-          //  console.log(data) 
-           setCoin(data)
-           setLoading(false)
-         } catch (error) {
-          console.log(error)
-          setLoading(false)
-         }
-       }
-       getCoin() 
-    },[id])
+  const [coin, setCoin] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const [currency, setCurrency] = useState("inr");
+  const currencySymbol = currency === "inr" ? "₹" : "$";
+  const profit = coin.market_data?.price_change_percentage_24h > 0;
+  useEffect(() => {
+    const getCoin = async () => {
+      try {
+        const { data } = await axios.get(`${Baseurl}/coins/${id}`);
+        //  console.log(data)
+        setCoin(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
+    getCoin();
+  }, [id]);
   return (
-  <>
-  {
-    loading ? <Loader/> : <> 
-         <div className=' coin-detail'  > 
-           <div className='coin-info'>
-           <div className='btn'>
-             <button onClick={()=>setCurrency('inr')} >inr</button>
-             <button onClick={()=>setCurrency('usd')}>usd</button>
-           </div>
-              <div className="time">
-              {coin.last_updated}
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className=" coin-detail">
+            <div className="coin-info">
+              <div className="btn">
+                <button onClick={() => setCurrency("inr")}>inr</button>
+                <button onClick={() => setCurrency("usd")}>usd</button>
               </div>
+              <div className="time">{formatISODate(coin.last_updated)}</div>
               <div className="coin-image">
                 <img height={"150px"} src={coin.image.large} alt="" />
               </div>
-              <div className="coin-name">
-              {coin.name}
-              </div>
+              <div className="coin-name">{coin.name}</div>
               <div className="coin-price">
-             {currencySymbol}  {coin.market_data.current_price[currency]}
+                {currencySymbol} {coin.market_data.current_price[currency]}
               </div>
               <div className="coin-profit">
-              {profit ? <BiSolidUpArrow color='green' /> : <BiSolidDownArrow color='red' />  }
-                {coin.market_data.price_change_percentage_24h} % 
+                {profit ? (
+                  <BiSolidUpArrow color="green" />
+                ) : (
+                  <BiSolidDownArrow color="red" />
+                )}
+                {coin.market_data.price_change_percentage_24h} %
               </div>
-              <div className='market-rank'>
-                <IoPulseOutline color='orange' /> 
-               #{coin.market_cap_rank}
+              <div className="market-rank">
+                <IoPulseOutline color="orange" />#{coin.market_cap_rank}
               </div>
-              <div className='coin-desc'>
-                <p>     {coin.description['en'].split('.')[0]} </p>
-        </div>
-           </div>
-            <CoinChart currency={currency} /> 
-         </div>
-    </> 
-  }
-  
-  </>
-  )
-}
+              <div className="coin-desc">
+                <p> {coin.description["en"].split(".")[0]} </p>
+              </div>
+            </div>
+            <CoinChart currency={currency} />
+          </div>
+        </>
+      )}
+    </>
+  );
+};
 
-export default CoinDetails
-
-
+export default CoinDetails;
